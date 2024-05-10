56fbebba121c21a32ddd7b606918fd4f69170cce
Axel Cabral <axel@flexpro.com.br>
2024-05-10 11:49:56 -0300

    test ajust

--- test/my_home_page_test.dart
+++ test/my_home_page_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets("It shouldn't be able to go to the next page using a lowest number than zero or zero", (WidgetTester tester) async {
+  testWidgets("It shouldn't be able to go to the next page using a lower number than zero or zero", (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: MyHomePage(title: 'Test')));
 
     // Encontrar o widget TextFormField pelo Key
@@ -38,7 +38,7 @@ void main() {
     expect(find.text('The number must be greater than zero.'), findsOneWidget);
   });
 
-  testWidgets("It shouldn't be able to go to the next page using a gratest number than 1302", (WidgetTester tester) async{
+  testWidgets("It shouldn't be able to go to the next page using a greater number than 1302", (WidgetTester tester) async{
     await tester.pumpWidget(const MaterialApp(home: MyHomePage(title: 'Test2')));
 
     // Encontrar o widget TextFormField pelo Key
